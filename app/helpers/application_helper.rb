5a7b0a01e84dc7383252420e7686f67d317d0943
beya765 <kenshinpower2000@yahoo.co.jp>
2020-03-25 14:18:23 +0000

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # L4.2: ページごとの完全なタイトルを返します。
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    # 引数で渡ってきた文字列が空のままなら、base_titleだけを使用
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,13 +9,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_url
     assert_response :success
-    # L3.24: <title>タグ内に「Home | Ruby～」文字列があるかチェック
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
   
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    # L3.24: <title>タグ内に「Help | Ruby～」文字列があるかチェック
     assert_select "title", "Help | #{@base_title}"
   end
   
